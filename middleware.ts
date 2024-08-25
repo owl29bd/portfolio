07df0bf9edb8fd09fb869ad4303112326308e2c5
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2024-08-25 21:29:52 +0600

    NextResponse.redirect in Middleware

---
+++ middleware.ts
@@ -0,0 +1,50 @@
+/**
+ * Middleware allows you to run code before a request is completed. 
+ * Then, based on the incoming request, redirect to a different URL using NextResponse.redirect. 
+ * This is useful if you want to redirect users based on a condition 
+ * (e.g. 
+ *      - authentication, 
+ *      - session management, 
+ * etc) 
+ * or have a large number of redirects.
+ */
+
+import { NextResponse, NextRequest } from 'next/server';
+import { get } from '@vercel/edge-config';
+// import { authenticate } from 'auth-provider';
+ 
+export function middleware(request: NextRequest) {
+//   const isAuthenticated = authenticate(request);
+  const isAuthenticated = true;
+ 
+  // If the user is authenticated, continue as normal
+  if (isAuthenticated) {
+    return NextResponse.next();
+  }
+ 
+  // Redirect to login page if not authenticated
+  return NextResponse.redirect(new URL('/login', request.url));
+}
+ 
+export const config = {
+  matcher: '/dashboard/:path*',
+};
+
+type RedirectEntry = {
+    destination: string
+    permanent: boolean
+  }
+   
+export async function middleware_scalable(request: NextRequest) {
+    const pathname = request.nextUrl.pathname
+    const redirectData = await get(pathname)
+
+    if (redirectData && typeof redirectData === 'string') {
+        const redirectEntry: RedirectEntry = JSON.parse(redirectData)
+        const statusCode = redirectEntry.permanent ? 308 : 307
+        return NextResponse.redirect(redirectEntry.destination, statusCode)
+    }
+
+    // No redirect found, continue without redirecting
+    return NextResponse.next()
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/**
+ * The `redirects` option in the `next.config.js` file allows you to redirect an incoming request path to a different destination path.
+ * This is useful when you change the URL structure of pages or have a list of redirects that are known ahead of time.
+ *
+ * `redirects` supports
+ *      - path,
+ *      - header,
+ *      - cookie, and
+ *      - query matching,
+ * giving you the flexibility to redirect users based on an incoming request.
+ */
+module.exports = {
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true,
+      },
+      // Wildcard path matching
+      {
+        source: "/blog/:slug",
+        destination: "/news/:slug",
+        permanent: true,
+      },
+    ];
+  },
+};
