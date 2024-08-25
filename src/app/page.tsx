8d7c4b707c842907a6b7de09b1fb40190e53dae0
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2024-08-25 19:27:20 +0600

    next-link <Link> component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 // `app/page.tsx` is the UI for the `/` URL
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Next.js",
 };
 export default function Page() {
-  return <h1>Hello, Home page!</h1>;
+  return (
+    <>
+      <h1>Hello, Home page!</h1>
+      <Link href="/dashboard">Dashboard</Link>
+    </>
+  );
 }
