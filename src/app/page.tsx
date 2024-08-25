e375bca2de13d3c65cf1219e81a8d043152ffa38
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2024-08-25 21:19:02 +0600

    redirect by useRouter from client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,30 @@
-// `app/page.tsx` is the UI for the `/` URL
+/**
+ * `app/page.tsx` is the UI for the `/` URL
+ */
+
+"use client";
+
 import type { Metadata } from "next";
 import Link from "next/link";
+/**
+ * If you need to redirect inside an event handler in a Client Component,
+ * you can use the `push` method from the `useRouter` hook.
+ */
+import { useRouter } from "next/router";
 
 export const metadata: Metadata = {
   title: "Next.js Template",
 };
 export default function Page() {
+  const router = useRouter();
+
   return (
     <body>
       <h1>Hello, Home page!</h1>
       <Link href="/dashboard">Dashboard</Link>
+      <button type="button" onClick={() => router.push("/dashboard")}>
+        Dashboard
+      </button>
     </body>
   );
 }
